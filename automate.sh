f8e3eb8b28a978be3de028d8c70e7be9c98169b8
AMS <111883236+AMS003010@users.noreply.github.com>
2024-01-25 22:51:32 +0530

    commit

---
+++ automate.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]; then
+  echo "you are not root"
+  exit 1
+fi
+echo " |---> Boi you're root"
+
+echo " |---> Updating..."
+sudo apt update
+
+echo " |---> Upgrading..."
+sudo apt upgrade
+
+sudo DEBIAN_FRONTEND=noninteractive apt install -yq wireshark
+echo " |---> Wireshark installed"
+
+sudo apt-get install net-tools
+echo " |---> Net tools installed"
+
+sudo apt-get install traceroute
+echo " |---> Traceroute installed"
+
+sudo apt-get install nmap -y
+echo " |---> Nmap installed"
